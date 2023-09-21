6dc5f06e4579f3baeb94e6035d1b21f243763403
ian <iqtang@uci.edu>
2023-09-21 09:58:09 -0700

    Unittest Added

--- test_format.py
+++ test_format.py
@@ -2,9 +2,10 @@ import unittest
 from synth_data_module import FormatOutput
 
 class FormatTest(unittest.TestCase):
+
     output_loc = 'output/'
     encounter_type = 'inpatient'
-    format = FormatOutput('010735', output_loc, encounter_type)
+    fmt = FormatOutput('010735', output_loc, encounter_type)
 
     def test_add_procedure(self):
         pass
@@ -13,13 +14,15 @@ class FormatTest(unittest.TestCase):
         pass
 
     def test_modify_row(self):
-        pass
+        testdict = [{'a': 1, 'b': 2, 'c': 3},
+                  {'a': 100, 'b': 200, 'c': 300},
+                  {'a': 1000, 'b': 2000, 'c': 3000}]
 
     def test_get_procedure_list(self):
-        self.assertEqual(len(format.get_procedure_list()), 48)
+        self.assertEqual(len(self.fmt.get_procedure_list()), 48)
 
     def test_get_diagnosis_list(self):
-        self.assertEqual(len(format.get_diagnosis_list()), 46)
+        self.assertEqual(len(self.fmt.get_diagnosis_list()), 46)
 
 
 if __name__ == '__main__':
